93229b954d8105e7ddff5941e94a0e9d03ee780f
rutik021 <92723663+rutik021@users.noreply.github.com>
2024-05-22 14:44:50 +0530

    added RewadValue

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -4,7 +4,7 @@ public class RewardValue {
 
     private static final double MILES_TO_CASH_RATE = 0.0035;
 
-    // Constructor that accepts a cash value12
+    // Constructor that accepts a cash value
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
         this.milesValue = cashValue / MILES_TO_CASH_RATE;
@@ -24,16 +24,5 @@ public class RewardValue {
     // Method to get the miles value
     public double getMilesValue() {
         return milesValue;
-    }
-
-    public static void main(String[] args) {
-        // Example usage
-        RewardValue rewardFromCash = new RewardValue(100.0);
-        System.out.println("Cash value: " + rewardFromCash.getCashValue());
-        System.out.println("Miles value: " + rewardFromCash.getMilesValue());
-
-        RewardValue rewardFromMiles = new RewardValue(10000);
-        System.out.println("Cash value: " + rewardFromMiles.getCashValue());
-        System.out.println("Miles value: " + rewardFromMiles.getMilesValue());
     }
 }
